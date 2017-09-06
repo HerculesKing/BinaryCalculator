a8f56691412dec107a960a783e423e87486b0a94
joda2802 <johannessteppe281@gmail.com>
2017-09-06 18:19:57 +0200

    bugfixes

--- dezizubin.js
+++ dezizubin.js
@@ -5,18 +5,18 @@ var suk = "";
 var x = 1
 var check = ""
 var check2 = 0
-var save = ""
+
 var y = 1
 //Called when application is started.
 function OnStart()
 {
-	//Create a layout with objects vertically centered.
+
 	layMain = app.CreateLayout( "linear", "FillXY" );	
 
-	//Create array to hold number buttons.
+
 	keys = [ 7,8,9, 4,5,6, 1,2,3, 0,"C" ];
 	
-	//Create text control for displaying sum.
+
 	txtSum = app.CreateText( sum, 0.8, 0.1 );
 	txtSum.SetTextSize( 42 );
 	txtSum.SetBackColor( "#ff222222" );
@@ -29,22 +29,22 @@ function OnStart()
 	txtSuk.SetMargins( 0, 0.1, 0, 0.05 );
 	layMain.AddChild( txtSuk );
 	
-	//Create first row of buttons.
+
 	lay1st = app.CreateLayout( "linear", "Horizontal" );	
 	for( i=0; i<3; i++ ) AddButton( lay1st, keys[i] );
 	layMain.AddChild( lay1st );
 	
-	//Create second row of buttons.
+
 	lay2nd = app.CreateLayout( "linear", "Horizontal" );	
 	for( i=3; i<6; i++ ) AddButton( lay2nd, keys[i] );
 	layMain.AddChild( lay2nd );
 	
-	//Create third row of buttons.
+
 	lay3rd = app.CreateLayout( "linear", "Horizontal" );	
 	for( i=6; i<9; i++ ) AddButton( lay3rd, keys[i] );
 	layMain.AddChild( lay3rd );
 	
-	//Create fourth row of buttons.
+
 	lay4th = app.CreateLayout( "Linear", "Horizontal" );
 	for( i=9; i<11; i++ ) AddButton(lay4th,keys[i] );
 	
@@ -52,16 +52,16 @@ function OnStart()
 
 
 	
-	//Create fifth row of buttons.
+
 	lay5th = app.CreateLayout( "linear", "Horizontal" );	
 	AddButton( lay5th, "=" );
 	layMain.AddChild( lay5th );
 
-	//Add layout to app.	
+	
 	app.AddLayout( layMain );
 }
 
-//Add a button to a given layout.
+
 function AddButton( lay, name )
 {
 	if( name=="=" ) w = 0.8; else w=0.2;
@@ -70,7 +70,7 @@ function AddButton( lay, name )
 	lay.AddChild( btn );
 }
 
-//Called when user presses number buttons.
+
 function btns_OnTouch()
 {
 	app.Vibrate( "0,100" );
@@ -83,7 +83,7 @@ function btns_OnTouch()
 	if( txt=="=" ) CalcResult();
 	
 	//Handle clear button.
-	else if( txt=="C" ) sum= "",check = "",suk="",check2="",	txtSuk.SetText(suk)
+	else if( txt=="C" ) sum= "",check = "",suk="",check2="",y=1,	txtSuk.SetText(suk);
 
 
 	//Handle other buttons.
@@ -97,7 +97,7 @@ function btns_OnTouch()
 
 //Calculate sum.
 function CalcResult()
-{
+{check = "",suk="",check2="",y=1,	txtSuk.SetText(suk);
 	
 	check2 = sum
 	while(check2>=1) 
